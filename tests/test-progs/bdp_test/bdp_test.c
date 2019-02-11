05f376abc77d2d0297feab5d6dabe33a550c7025
Joao Miguel Morgado Pereira Vieira <u6022787@lnissrv2.eng.utah.edu>
2019-02-11 10:07:31 -0700

    Minor changes

---
+++ tests/test-progs/bdp_test/bdp_test.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <stdio.h>
+
+uint64_t binary_dot_product_hw(uint64_t data, uint64_t kernel_address) {
+  uint64_t result = 0;
+
+  // move data to registers
+  __asm("MOV X9,  %[data]" : : [data] "r" (data));
+  __asm("MOV X10, %[data]" : : [data] "r" (kernel_address));
+
+  // perform operation
+  __asm(".long 0b00000001000010100000000100101001");
+  //             |||        | rm||t||i|| rn|| rd|
+
+  // store data back
+  __asm("MOV %[result], X9" : [result] "=r" (result) : );
+
+  return result;
+}
+
+void print_binary(uint64_t num) {
+  for (int i = 0; i < sizeof(uint64_t) * 8; i++) {
+    if (i != 0 && i % 4 == 0)
+      printf(" ");
+
+    printf("%d", (num & 0x8000000000000000) == 0 ? 0 : 1);
+    num <<= 1;
+  }
+}
+
+int main() {
+  uint64_t n1 = 0xffffffffffffffff;
+  uint64_t n2 = 0xffffffffffffffff;
+
+  print_binary(binary_dot_product_hw(n1, n2));
+  printf("\n");
+
+  return 0;
+}
