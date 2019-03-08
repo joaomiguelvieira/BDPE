df3fcce1c2decf6b40763511279c700e4577e9fd
Joao Miguel Morgado Pereira Vieira <u6022787@lnissrv2.eng.utah.edu>
2019-03-08 13:24:42 -0700

    Corrected bug in profiling

--- profile_reram.py
+++ profile_reram.py
@@ -17,5 +17,28 @@ args = parser.parse_args()
 
 reram_file = open(args.file, "r")
 
+reram = {}
+cache = {}
 for line in reram_file:
-	[] = line.rstrip().split(" ")
+	data = numpy.uint64(int(line.rstrip().split(" ")[-1].split(";")[0].split("x")[1], 16))
+	address = numpy.uint64(line.split("[")[1].split("]")[0])
+	
+	reram[address] = data
+
+	cache_addr = address & numpy.uint64(0x7f)
+	if cache_addr not in cache:
+		cache[cache_addr] = []
+	cache[cache_addr].append(hex(address))
+
+min_addr = min(reram.keys())
+max_addr = max(reram.keys())
+addr_space = max_addr - min_addr
+n_entries = (addr_space / 8) + 1
+
+print Tags.INFO + "Lower cached address is {}".format(min_addr)
+print Tags.INFO + "Higher cached address is {}".format(max_addr)
+print Tags.INFO + "Cached address space is {} bytes, or {} entries".format(addr_space, n_entries)
+
+for key, value in cache.iteritems():
+	if len(value) > 1:
+		print Tags.WARNING + "Detected colision for addresses {}".format(value)
