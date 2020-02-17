eb85182a0a0d13b61d6032db5a2fac6e173de247
joaomiguelvieira <joaomiguelvieira@live.com.pt>
2020-02-17 16:56:46 +0000

    Added matlab scripts

--- analyze_results.m
+++ analyze_results.m
@@ -10,19 +10,19 @@ number_of_bdp         = 42658304;
 % extract information from the gem5 output file
 stats = fopen("darknet_baseline_separate_file_out/stats.txt");
 
-line = fgetl(stats);
-while ischar(line)
-    if contains(line, "system.cpu.dcache.ReadReq_hits::total")
-        words = strsplit(line);
+aux = fgetl(stats);
+while ischar(aux)
+    if contains(aux, "system.cpu.dcache.ReadReq_hits::total")
+        words = strsplit(aux);
         cache_hits_baseline = str2double(words(2));
     end
 
-    if contains(line, "system.cpu.dcache.ReadReq_misses::total")
-        words = strsplit(line);
+    if contains(aux, "system.cpu.dcache.ReadReq_misses::total")
+        words = strsplit(aux);
         cache_misses_baseline = str2double(words(2));
     end
 
-    line = fgetl(stats);
+    aux = fgetl(stats);
 end
 
 mem_accesses_baseline = cache_hits_baseline + cache_misses_baseline;
@@ -42,19 +42,19 @@ for i = 1:length(hit_rates)
     filename = strcat("m5out_accelerated_hit_rate_", hit_rates(i), "/stats.txt");
     stats = fopen(filename);
     
-    line = fgetl(stats);
-    while ischar(line)
-        if contains(line, "system.cpu.dcache.ReadReq_hits::total")
-            words = strsplit(line);
+    aux = fgetl(stats);
+    while ischar(aux)
+        if contains(aux, "system.cpu.dcache.ReadReq_hits::total")
+            words = strsplit(aux);
             cache_hits(i) = str2double(words(2));
         end
         
-        if contains(line, "system.cpu.dcache.ReadReq_misses::total")
-            words = strsplit(line);
+        if contains(aux, "system.cpu.dcache.ReadReq_misses::total")
+            words = strsplit(aux);
             cache_misses(i) = str2double(words(2));
         end
         
-        line = fgetl(stats);
+        aux = fgetl(stats);
     end
     
     mem_accesses(i) = cache_hits(i) + cache_misses(i);
@@ -63,8 +63,98 @@ for i = 1:length(hit_rates)
     fclose(stats);
 end
 
-plot.hit_rates     = str2double(hit_rates(1:length(hit_rates) - 1));
-plot.mem_spares    = mem_spare(1:length(mem_spare) - 1);
-plot.timings       = [1700.376, 1694.860, 1695.145, 1695.077, 1695.238];
-plot.cache_misses  = [cache_misses_baseline; cache_misses(1:length(cache_misses) - 1)];
-plot.xcache_misses = ["Baseline", hit_rates(1:length(hit_rates) - 1)];
+graphs.hit_rates     = str2double(hit_rates(1:length(hit_rates) - 1));
+graphs.mem_spares    = mem_spare(1:length(mem_spare) - 1);
+graphs.timings       = [1917.167, 1700.376, 1694.860, 1695.145, 1695.077, 1695.238];
+graphs.cache_misses  = [cache_misses_baseline; cache_misses(1:length(cache_misses) - 1)];
+graphs.xcache_misses = ["Base", hit_rates(1:length(hit_rates) - 1)];
+
+% memory accesses spares
+figure('Position', [699, 259, 280, 250]);
+bar(graphs.mem_spares);
+ylim([0, 0.6]);
+yticks(linspace(0, 0.6, 7));
+%ylim([min(graphs.mem_spares) - (max(graphs.mem_spares) - min(graphs.mem_spares)) * .1, max(graphs.mem_spares) + (max(graphs.mem_spares) - min(graphs.mem_spares)) * .1]);
+%yticks(linspace(min(graphs.mem_spares) - (max(graphs.mem_spares) - min(graphs.mem_spares)) * .1, max(graphs.mem_spares) + (max(graphs.mem_spares) - min(graphs.mem_spares)) * .1, 7))
+set(gca, 'xticklabel', graphs.hit_rates);
+ytickformat('%.2f');
+ylabel({"Relative memory"; "accesses reduction"});
+xlabel("BPDE usage rate [%]");
+saveas(gcf,'mem_spares.eps', 'epsc');
+
+% timings
+figure('Position', [699, 259, 280, 250]);
+yyaxis left
+bar(graphs.timings);
+%ylim([0, max(graphs.timings) * 1.1]);
+%ylim([min(graphs.timings) - (max(graphs.timings) - min(graphs.timings)) * .1, max(graphs.timings) + (max(graphs.timings) - min(graphs.timings)) * .1]);
+ylim([1672,1936]);
+%yticks(linspace(0, max(graphs.timings) * 1.1, 7))
+%yticks(linspace(min(graphs.timings) - (max(graphs.timings) - min(graphs.timings)) * .1, max(graphs.timings) + (max(graphs.timings) - min(graphs.timings)) * .1, 7))
+yticks(linspace(1672,1936,7));
+%xlim([min(graphs.hit_rates), max(graphs.hit_rates)]);
+%ytickformat('%.2f');
+ylabel("Inference time [ms]");
+yyaxis right
+plot([1 2 3 4 5 6], abs(graphs.timings - graphs.timings(1)) * 100 / graphs.timings(1), '-o');
+ylim([0, 12]);
+yticks(linspace(0, 12, 7));
+set(gca, 'xticklabel', graphs.xcache_misses);
+%xticks(graphs.hit_rates);
+ylabel("Performance gains [%]");
+%annotation('doublearrow', [], []);
+xlabel("BPDE usage rate [%]");
+saveas(gcf, 'timings.eps', 'epsc');
+
+% cache misses
+figure('Position', [699, 259, 280, 300]);
+bar(graphs.cache_misses);
+ylim([min(graphs.cache_misses) - (max(graphs.cache_misses) - min(graphs.cache_misses)) * .1, max(graphs.cache_misses) + (max(graphs.cache_misses) - min(graphs.cache_misses)) * .1]);
+yticks(linspace(min(graphs.cache_misses) - (max(graphs.cache_misses) - min(graphs.cache_misses)) * .1, max(graphs.cache_misses) + (max(graphs.cache_misses) - min(graphs.cache_misses)) * .1, 7))
+set(gca, 'xticklabel', graphs.xcache_misses);
+ytickformat('%.5f');
+ylabel("L1 data cache misses");
+xlabel("BPDE usage rate [%]");
+
+annotation('line', [.32, .42], [.86, .86], 'LineStyle', '--');
+annotation('doublearrow', [.37, .37], [.19, .86]);
+annotation('textbox',[.28, .44, .25, .25],'String','\approx 0.01%','FitBoxToText','on','BackgroundColor','white');
+
+saveas(gcf, 'cache_misses.eps', 'epsc');
+
+%% Plot energy results
+energy_results      = xlsread("power_calculation_fdsoi.xlsx", "Sheet1", "B14:H14");
+energy_results_dram = xlsread("power_calculation_fdsoi.xlsx", "Sheet1", "B13:H13");
+
+graphs.energy_results      = [energy_results(length(energy_results)), energy_results(1:length(energy_results) - 2)];
+graphs.energy_results_dram = [energy_results_dram(length(energy_results_dram)), energy_results_dram(1:length(energy_results_dram) - 2)];
+
+figure('Position', [699, 259, 280, 300]);
+bar(graphs.energy_results);
+ylim([min(graphs.energy_results) - (max(graphs.energy_results) - min(graphs.energy_results)) * .1, max(graphs.energy_results) + (max(graphs.energy_results) - min(graphs.energy_results)) * .1]);
+yticks(linspace(min(graphs.energy_results) - (max(graphs.energy_results) - min(graphs.energy_results)) * .1, max(graphs.energy_results) + (max(graphs.energy_results) - min(graphs.energy_results)) * .1, 7))
+set(gca, 'xticklabel', graphs.xcache_misses);
+ytickformat('%.3f');
+ylabel({"Total energy spent"; "excluding DRAM [J]"});
+xlabel("BPDE usage rate [%]");
+
+annotation('line', [.42, .52], [.859, .859], 'LineStyle', '--');
+annotation('doublearrow', [.458, .458], [.205, .859]);
+annotation('textbox',[.43, .25, .2, .35],'String','7.4%','FitBoxToText','on','BackgroundColor','white');
+
+saveas(gcf, 'energy_results.eps', 'epsc');
+
+figure('Position', [699, 259, 280, 300]);
+bar(graphs.energy_results_dram);
+ylim([min(graphs.energy_results_dram) - (max(graphs.energy_results_dram) - min(graphs.energy_results_dram)) * .1, max(graphs.energy_results_dram) + (max(graphs.energy_results_dram) - min(graphs.energy_results_dram)) * .1]);
+yticks(linspace(min(graphs.energy_results_dram) - (max(graphs.energy_results_dram) - min(graphs.energy_results_dram)) * .1, max(graphs.energy_results_dram) + (max(graphs.energy_results_dram) - min(graphs.energy_results_dram)) * .1, 7))
+set(gca, 'xticklabel', graphs.xcache_misses);
+ytickformat('%.3f');
+ylabel("Total energy spent [J]");
+xlabel("BPDE usage rate [%]");
+
+annotation('line', [.37, .47], [.859, .859], 'LineStyle', '--');
+annotation('doublearrow', [.42, .42], [.205, .859]);
+annotation('textbox',[.38, .25, .2, .35],'String','40.51 mJ','FitBoxToText','on','BackgroundColor','white');
+
+saveas(gcf, 'energy_results_dram.eps', 'epsc');
---
+++ yolov3_profiling.m
@@ -0,0 +1,44 @@
+%% Initialize
+clearvars;
+clc;
+close all;
+
+%% Build workspace
+call_freq = fopen("call_freq.txt");
+
+segs = 135200;
+graphs.addrs = zeros(segs, 1);
+graphs.freq  = zeros(segs, 1);
+iter = 0;
+
+aux = fgetl(call_freq);
+while ischar(aux)
+    words = strsplit(aux);
+    
+    iter = iter + 1;
+    graphs.addrs(iter) = str2double(words(1));
+    graphs.freq(iter)  = str2double(words(2));
+
+    aux = fgetl(call_freq);
+end
+
+save("yolov3_profiling.mat");
+
+%% Plot results 
+load yolov3_profiling.mat;
+
+figure('Position', [699, 259, 560, 300]);
+bar(graphs.freq(1:4000), 'r'); hold on;
+bar([zeros(4000, 1); graphs.freq(4001:end)], 'b')
+set(gca,'yscale','log')
+xlabel("Kernel relative address");
+ylabel("Number of uses");
+saveas(gcf, 'kernel_usage_all.eps', 'epsc');
+
+figure('Position', [699, 259, 280, 300]);
+bar(graphs.freq(1:4000), 'r')
+set(gca,'yscale','log')
+xlabel("Kernel relative address");
+ylabel("Number of uses");
+set(gca, 'YGrid', 'on', 'XGrid', 'off');
+saveas(gcf, 'kernel_usage_zoom.eps', 'epsc');
